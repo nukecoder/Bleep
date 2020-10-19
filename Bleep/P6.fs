5461ee241d1a474379f29d26429f627caa582e2a
nukecoder <ellisgen@gmail.com>
2020-10-19 05:58:33 -0500

    Completed Working example.

---
+++ Bleep/P6.fs
@@ -0,0 +1,115 @@
+﻿module P6
+//#r "System.ServiceModel"
+//#r "FSharp.Data.TypeProviders.dll"
+//open Microsoft.FSharp.Data.TypeProviders
+open FSharp.Data.TypeProviders
+open System.Linq.Expressions
+open System
+open System.ServiceModel
+open System.Xml
+open System.ServiceModel.Channels
+open System.ServiceModel.Security
+
+type P6Wsdl = WsdlService<"http://p6ws.cpnpp.net:8206/p6ws/services/ProjectService?wsdl">
+let un = "xb0g"
+let pw = "Granite2019"
+
+let randomString len =
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let charsLen = chars.Length
+    let random = System.Random()
+    let randomChars = [| for i in 0 .. len -> chars.[random.Next(charsLen)]|]
+    new System.String(randomChars)
+
+let nonceString (str : string) =
+    let sha1 = System.Security.Cryptography.SHA1.Create()
+    let myba = System.Text.UnicodeEncoding.UTF8.GetBytes(str + (randomString 10))
+    let hash = sha1.ComputeHash myba
+    (Convert.ToBase64String hash).Substring(0,22) + "=="
+
+//#region AWSexample
+let soapSig op =
+    let dtn = DateTime.UtcNow
+    let dt = new DateTime(dtn.Year,dtn.Month,dtn.Day,dtn.Hour,dtn.Minute,dtn.Second,1)
+    let ts = XmlConvert.ToString(dt,XmlDateTimeSerializationMode.Utc)
+    let str = sprintf "AmazonS3%s%s" op ts
+    let sigStr = nonceString str
+    sigStr,dt
+
+let wssClient url un pw =
+    let b = new CustomBinding()
+    // security
+    let security = TransportSecurityBindingElement.CreateUserNameOverTransportBindingElement()
+    security.AllowInsecureTransport <- true
+    security.IncludeTimestamp <- false
+    security.DefaultAlgorithmSuite <- SecurityAlgorithmSuite.Basic256
+    security.MessageSecurityVersion <- MessageSecurityVersion.WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10
+
+    // message encoding
+    let encoding = new TextMessageEncodingBindingElement()
+    encoding.MessageVersion = MessageVersion.Soap11
+
+    // transport
+    let transport = new HttpTransportBindingElement()
+    //let transport = new BasicHttpBinding(BasicHttpSecurityMode.Transport)
+    transport.MaxReceivedMessageSize <- 10000000L // 10 megs
+
+    b.Elements.Add security
+    b.Elements.Add encoding
+    b.Elements.Add transport
+
+    // endpoint
+    let epr = new EndpointAddress("http://p6ws.cpnpp.net:8206/p6ws/services/ProjectService")
+    let client = new P6Wsdl.ServiceTypes.ProjectPortTypeClient(b,epr)
+    client.ChannelFactory.Endpoint.Behaviors.Remove(System.ServiceModel.Description.ClientCredentials())
+    //client.ChannelFactory.Endpoint.Behaviors.Remove(System.ServiceModel.Description.ClientCredentials)
+    client.ChannelFactory.Endpoint.Behaviors.Add(new CustomCredentials())
+    client.ClientCredentials.UserName.UserName <- un
+    client.ClientCredentials.UserName.Password <- pw
+    client
+
+let runOp op f =
+    //let b = new BasicHttpBinding(BasicHttpSecurityMode.Transport)
+    //b.MaxReceivedMessageSize <- 10000000L
+    use ser = wssClient "http://p6ws.cpnpp.net:8206/p6ws/services/ProjectService" "xb0g" "Granite2019"
+    try
+        let readProjs = new P6Wsdl.ServiceTypes.ReadProjects()
+        readProjs.Filter <- "ObjectId = 3743"
+        readProjs.Field <- [| P6Wsdl.ServiceTypes.ProjectFieldType.Id; P6Wsdl.ServiceTypes.ProjectFieldType.Name; P6Wsdl.ServiceTypes.ProjectFieldType.DataDate |]
+        let sign,ts = soapSig op
+        let resp = f ser sign ts
+        let projects = ser.ReadProjects(readProjs)
+        for p in projects
+            do printfn "%A" p.Name
+        ser.Close()
+        resp
+    with ex ->
+        ser.Abort()
+        raise ex
+//#endregion
+
+
+let P6Client = P6Wsdl.GetProjectPort()
+P6Client.DataContext.ClientCredentials.UserName.UserName <- "xb0g"
+P6Client.DataContext.ClientCredentials.UserName.Password <- "Granite2019"
+
+
+
+type ProjId = 
+    { Id : int }
+
+try
+    let readProjs = new P6Wsdl.ServiceTypes.ReadProjects()
+    readProjs.Filter <- "ObjectId = 3743"
+    readProjs.Field <- [| P6Wsdl.ServiceTypes.ProjectFieldType.Id; P6Wsdl.ServiceTypes.ProjectFieldType.Name; P6Wsdl.ServiceTypes.ProjectFieldType.DataDate |]
+    
+    //let req = P6Wsdl.ServiceTypes.ReadProjectsRequest(readProjs)
+    let res = P6Client.ReadProjects(readProjs)
+    printfn "%A" res
+with 
+    | ex ->
+        let rec inner (ex : Exception) =
+            if ex.InnerException <> null then
+                inner ex.InnerException
+            printfn "%s" ex.Message
+        inner ex
---
+++ Bleep/Program.fs
@@ -0,0 +1,161 @@
+﻿// Exercise in creating an Elmish.WPF applciation
+// adapted from Racket example: https://dev.to/goober99/learn-racket-by-example-gui-programming-3epm
+open System
+open Elmish
+open Elmish.WPF
+open BleepViews
+open System.Threading
+open System.Windows
+
+// Globals
+// scale used by slider
+let minPosition = 0
+let maxPosition = 20000
+
+// rage of frequencies used by System.Console.Beep1
+let minFrequency = 37.0
+let maxFrequency = 32767.0
+
+// logarithmic scale for frequency (so middle A [440Hz] falls about in the middle)
+let minFreq = minFrequency |> float |> Math.Log
+let maxFreq = maxFrequency |> float |> Math.Log
+let frequencyScale = (maxFreq - minFreq) / float (maxPosition - minPosition)
+
+// Convert the slider position to frequency
+let sliderPosToFreq pos =
+    Math.Exp (minFreq + frequencyScale * (double pos))
+// Convert the frequency to slider position
+let freqToSliderPos freq =
+    ((Math.Log (double freq)) - (double minFreq)) / (frequencyScale + (double minPosition)) |> Math.Round
+
+// Validation Fucntions
+let requireNotEmpty s =
+  if String.IsNullOrEmpty s then Error "This field is required" else Ok s
+
+let parseInt (s:string) =
+    match Int32.TryParse s with
+    | true,i when i <= 0 -> Error "Please enter a positive integer."
+    | true,i -> Ok i
+    | false,_ -> Error "Please enter a valid integer."
+
+let validateDuration =
+  requireNotEmpty
+  >> Result.bind parseInt
+
+// Some Notes!
+let notes =
+    [ "A", 440.00
+    ; "B", 493.88
+    ; "C", 261.33
+    ; "D", 293.66
+    ; "E", 329.63
+    ; "F", 349.23
+    ; "G", 392.00 // the sources page lists this as 292, but their source lists it as 392.
+    ] |> Map.ofList
+
+
+// MODEL
+type Model =
+  { Frequency : double
+    Duration : int 
+    DurationRawValue : string
+    SelectedNote : string
+    PlayInProgress : bool
+    }
+
+let initialNote = "A"
+let init () = { Frequency = Map.find initialNote notes
+                Duration = 2000
+                DurationRawValue = "2000"
+                SelectedNote = initialNote
+                PlayInProgress = false},
+                []
+
+type CmdMsg =
+    | Play of double * int
+
+type Msg =
+    | Lower
+    | Raise
+    | RequestPlay
+    | SetFrequency of double
+    | SetDuration of int
+    | PlayFinished
+    | DurationFieldUpdate of string
+    | FrequencyFieldUpdate of string
+    | SelectNewNote of string
+    | DoNothing
+
+let updateFrequency f m =
+    {m with Frequency = match f with
+                        | f when f > maxFrequency -> maxFrequency
+                        | f when f < minFrequency -> minFrequency
+                        | _ -> f }
+
+
+// UPDATE
+let update msg m =
+    match msg with
+    | Lower -> updateFrequency (m.Frequency / 2.0) m, []
+    | Raise -> updateFrequency (m.Frequency * 2.0) m, []
+    | RequestPlay -> {m with PlayInProgress = true }, [Play (m.Frequency, m.Duration)]
+    | SetFrequency f -> updateFrequency f m, []
+    | SetDuration i -> {m with Duration = i}, []
+    | PlayFinished -> {m with PlayInProgress = false }, []
+    | DurationFieldUpdate s -> match parseInt s with
+                               | Ok i -> {m with DurationRawValue = s; Duration = i}, []
+                               | _ -> {m with DurationRawValue = s}, []
+    | FrequencyFieldUpdate s -> match System.Double.TryParse s with
+                                | true,d -> updateFrequency d m,[]
+                                | _ -> m,[]
+    | SelectNewNote n -> { m with SelectedNote = n} |> updateFrequency (Map.find n notes), []
+    | DoNothing -> {m with PlayInProgress = false }, []
+
+
+// VIEW
+let bindings () =
+  [
+    "Frequency" |> Binding.twoWay(
+      (fun m -> string m.Frequency),
+      (fun newVal m -> newVal |> FrequencyFieldUpdate))
+    "Lower" |> Binding.cmd (fun m -> Lower)
+    "Raise" |> Binding.cmd (fun m -> Raise)
+    "Play" |> Binding.cmdIf (
+        (fun m -> RequestPlay),
+        (fun m -> not m.PlayInProgress)
+    )
+    "Duration" |> Binding.twoWayValidate (
+      (fun m -> string m.DurationRawValue),
+      DurationFieldUpdate,
+      (fun m -> validateDuration m.DurationRawValue)
+    )
+    "SliderPosition" |> Binding.twoWay(
+      (fun m -> (float (freqToSliderPos m.Frequency))),
+      (fun newVal m -> sliderPosToFreq (double newVal) |> SetFrequency))
+    "Notes" |> Binding.oneWay (fun m -> notes)
+    "SelectedNote" |> Binding.twoWay(
+        (fun m -> m.SelectedNote),
+        (fun newNote m -> SelectNewNote newNote))
+      ]
+
+
+// COMMANDS
+let play (freq,dur) = 
+    Application.Current.Dispatcher.Invoke(fun () ->
+      async {
+        do! Async.SwitchToNewThread ()
+        Console.Beep(freq,dur)
+      }
+    )
+
+let toCmd = function
+    | Play(f,d) -> Cmd.OfAsync.perform play (int f,d) (fun _ -> PlayFinished)
+
+[<EntryPoint; STAThread>]
+let main argv = 
+    Program.mkProgramWpfWithCmdMsg
+        init update bindings toCmd 
+        |> Elmish.Program.withConsoleTrace
+        |> Program.runWindowWithConfig
+          { ElmConfig.Default with LogConsole = true; Measure = true }
+          (MainWindow())
